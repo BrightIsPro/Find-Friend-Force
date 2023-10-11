4d4cb6f4f824c13a1626ec79138629db78249264
dewakuneiei <dewakuneiei@gmail.com>
2023-10-11 19:39:16 +0700

    PUT DELETE
    Version

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const apiPath = "http://localhost:8000/";
 const partyNameInput = document.getElementById("01");
+const input_party_name = document.getElementById('input-party-name')
 
 // COntain all data 
 var allData;
@@ -9,16 +10,14 @@ const destination = document.getElementById("incontent1");
 const templateElement = temp.cloneNode(true);
 temp.remove()
 
-const sumbit_party_elm = document.getElementById('clickcreate')
-
 // Make a GET request to the API endpoint
 fetch(`${apiPath}party`, {
-  method: 'GET',
-  headers: {
-    'Accept': 'application/json',
-    // You may need to include additional headers, such as authentication tokens.
-  },
-})
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      // You may need to include additional headers, such as authentication tokens.
+    },
+  })
   .then((response) => {
     if (response.ok) {
       return response.json();
@@ -36,27 +35,174 @@ fetch(`${apiPath}party`, {
   });
 
 
-/*--------------------------------------------------*/
-
 function readData(data) {
-  for(let i = 0; i < data.length; i++) {
+  let length = data.length - 1
+  for (let i = length; i > 0; i--) {
     let clone = templateElement.cloneNode(true);
     destination.appendChild(clone);
-    clone.id = "clon" + i
+    clone.id = "clone" + i
+    let ab = clone.querySelector('.bright1 > #ab1')
+    ab.innerHTML = data[i].party_name
+    ab.id = "clild" + clone
+
+    let edit_btn = clone.querySelector('.dotdotdot > .method_edit');
+    let delete_btn = clone.querySelector('.dotdotdot > .method_delete');
+
+    edit_btn.addEventListener('click', () => {
+      onClickedEdit(data[i].party_id)
+    })
+    delete_btn.addEventListener('click', () => {
+      onClickedDelete(data[i].party_id)
+    })
   }
-
-  update()
-
 
 }
 
-function update() {
-  const bright11 = document.getElementsByClassName("bright11");
+//----------- PUT and DELETE
+async function onClickedEdit(id) {
+
+  // change data here
+  // change data here
+  const newData = {
+    party_name: "Hello WOrld" // Change here
+  }
   
-  for(let i = 0; i < bright11.length; i++) {
-    bright11[i].innerHTML = allData[i].party_name;
+  await fetch(`${apiPath}party/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newData),
+  })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Failed to update data');
+      }
+    })
+    .then(data => {
+      console.log('Data updated:', data.message);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+
+  location.reload()
+}
+
+async function onClickedDelete(id) {
+  console.log(id);
+  await fetch(`${apiPath}party/${id}`, {
+    method: 'DELETE',
+  })
+    .then(response => {
+      if (response.ok) {
+        // The resource was successfully deleted
+        console.log('Resource deleted successfully');
+      } else {
+        throw new Error('Failed to delete data');
+      }
+    })
+    .catch(error => {
+      // Handle any errors that occurred during the request
+      console.error('Error:', error);
+    });
+
+    location.reload()
+
+}
+
+
+/*--------------------------------------------------*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// CREATE
+const sumbit_party_elm = document.getElementById('clickcreate')
+sumbit_party_elm.addEventListener('click', async function () {
+  // Make a GET request to the API endpoint
+
+  if (input_party_name.value == "") {
+    return;
   }
 
+  await postParty(input_party_name);
+});
+
+
+const postParty = function (element) {
+  fetch(apiPath + "party", {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json', // Specify the content type
+        // You may need to include additional headers, such as authentication tokens.
+      },
+      body: JSON.stringify({ // Convert the JavaScript object to a JSON string
+        "party_id": 1,
+        "user_id": 1,
+        "party_name": element.value
+      })
+    })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Failed to fetch data');
+      }
+    })
+    .then((data) => {
+      // Process and use the data here
+      location.reload();
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
 
 }
 
@@ -70,41 +216,3 @@ function update() {
 
 
 
-
-
-
-
-
-
-
-
-sumbit_party_elm.addEventListener('click', function() {
-// Make a GET request to the API endpoint
-
-})
-
-fetch(`${apiPath}party`, {
-  method: 'POST',
-  headers: {
-    'Accept': 'application/json',
-    // You may need to include additional headers, such as authentication tokens.
-  },
-  body: {
-    user_id: 1,
-    party_name: "123"
-  }
-})
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error('Failed to fetch data');
-    }
-  })
-  .then((data) => {
-    // Process and use the data here
-    
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });  
